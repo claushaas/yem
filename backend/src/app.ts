756b66077bf511a4fd238e25336d16b7d76892d4
Claus Haas <claus.haas@me.com>
2024-02-16 13:32:24 -0300

    lint correction

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,28 +1,28 @@
-import express = require('express');
+import express from 'express';
 import 'express-async-errors';
-import { Request, Response, NextFunction } from 'express';
+import {type Request, type Response, type NextFunction} from 'express';
 
 export default class App {
-  public app: express.Express;
+	public app: express.Express;
 
-  constructor() {
-    this.app = express();
+	constructor() {
+		this.app = express();
 
-    this.initializeMiddlewares();
-    this.initializeErrorHandling();
-  }
+		this.initializeMiddlewares();
+		this.initializeErrorHandling();
+	}
 
-  public start(PORT: string | number): void {
-    this.app.listen(PORT, () => console.log(`Running on port ${PORT}`));
-  }
+	public start(PORT: string | number): void {
+		this.app.listen(PORT, () => {
+			console.log(`Running on port ${PORT}`);
+		});
+	}
 
-  private initializeMiddlewares() {
-    this.app.use(express.json());
-  }
+	private initializeMiddlewares() {
+		this.app.use(express.json());
+	}
 
-  private initializeErrorHandling() {
-    this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-      return res.status(500).send(err.message);
-    });
-  }
+	private initializeErrorHandling() {
+		this.app.use((err: Error, req: Request, res: Response, next: NextFunction) => res.status(500).send(err.message));
+	}
 }
