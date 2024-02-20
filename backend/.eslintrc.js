f7a104ef49ac9ab60825c781f760d6bcae96cf25
Claus Haas <claus.haas@me.com>
2024-02-20 08:32:06 -0300

    alter eslint configs

---
+++ backend/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+	root: true,
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: 'xo',
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [
+				'.eslintrc.{js,cjs}',
+			],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+		{
+			extends: [
+				'xo-typescript',
+			],
+			files: [
+				'*.ts',
+				'*.tsx',
+			],
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: './tsconfig.json',
+		tsconfigRootDir: __dirname,
+
+	},
+	rules: {
+		'new-cap': ['error', {capIsNew: false}],
+	},
+};
