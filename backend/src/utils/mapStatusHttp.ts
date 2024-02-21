473bdd39b7b9208fa3754091d1c890df4e89450b
Claus Haas <claus.haas@me.com>
2024-02-21 09:31:20 -0300

    add map http status util

---
+++ backend/src/utils/mapStatusHttp.ts
@@ -0,0 +1,11 @@
+export default function mapStatusHttp(status: string): number {
+	switch (status) {
+		case 'SUCCESSFUL': return 200;
+		case 'CREATED': return 201;
+		case 'INVALID_DATA': return 400;
+		case 'NOT_FOUND': return 404;
+		case 'UNAUTHORIZED': return 401;
+		case 'UNPROCESSABLE_ENTITY': return 422;
+		default: return 500;
+	}
+}
