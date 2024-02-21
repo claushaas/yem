1e8e159d760bf1dd8f0c2401ee45ef6b8e137135
Claus Haas <claus.haas@me.com>
2024-02-21 09:54:29 -0300

    add verify role middleware do create course route

--- backend/src/routes/course.router.ts
+++ backend/src/routes/course.router.ts
@@ -1,13 +1,18 @@
-import {type Request, Router, type Response} from 'express';
+import {
+	type Request, Router, type Response, type NextFunction,
+} from 'express';
 import CourseController from '../controllers/course.controller';
 import validateAuthToken from '../middlewares/validateAuthToken.middleware';
+import verifyRole from '../middlewares/verifyRole.middleware';
 
 const courseRouter = Router();
 
-courseRouter.use(validateAuthToken);
-
 courseRouter.post(
 	'/',
+	validateAuthToken,
+	(req: Request, res: Response, next: NextFunction) => {
+		verifyRole(req, res, next, 'admin');
+	},
 	async (req: Request, res: Response) => {
 		await new CourseController().create(req, res);
 	},
