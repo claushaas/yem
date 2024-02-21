e85f94a241150acdfbf86ab72ee3305b43edefb1
Claus Haas <claus.haas@me.com>
2024-02-21 09:28:04 -0300

    refactor course router endpoint

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,8 +1,10 @@
 import {Router} from 'express';
 import courseRouter from './course.router';
+import userRouter from './user.router';
 
 const router = Router();
 
-router.use('/courser', courseRouter);
+router.use('/courses', courseRouter);
+router.use('/users', userRouter);
 
 export default router;
