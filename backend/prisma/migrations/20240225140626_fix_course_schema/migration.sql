e5083e46443fce8d7e459876a29f5d7cb045bdd5
Claus Haas <claus.haas@me.com>
2024-02-25 14:12:17 -0300

    fix prisma schema and migrations

--- backend/prisma/migrations/20240225140626_fix_course_schema/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `videoSouceUrl` on the `courses` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "courses" DROP COLUMN "videoSouceUrl",
-ADD COLUMN     "videoSourceUrl" TEXT;
