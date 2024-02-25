bdeae50f51ad9ef5493d5ee46af155f63a865558
Claus Haas <claus.haas@me.com>
2024-02-25 13:56:21 -0300

    add getAll method to course service

--- backend/src/services/course.service.ts
+++ backend/src/services/course.service.ts
@@ -1,5 +1,8 @@
 import CourseModel from '../models/course.model';
 import {type Prisma} from '@prisma/client';
+import testUserRoles from '../utils/testUserRoles';
+import type Role from '../types/Role';
+import {type UserRoles} from '../types/User';
 
 export default class CourseService {
 	private readonly _model: CourseModel;
@@ -14,6 +17,30 @@ export default class CourseService {
 		return {
 			status: 'CREATED',
 			data: createdCourse,
+		};
+	}
+
+	public async getAll(userRoles: UserRoles = []) {
+		if (userRoles.includes('admin')) {
+			const courses = await this._model.getAll();
+
+			return {
+				status: 'SUCCESSFUL',
+				data: courses,
+			};
+		}
+
+		const rawCourses = await this._model.getPublished();
+
+		const returnableCourses = rawCourses.map(course => ({
+			...course,
+			content: testUserRoles(course.roles as Role[], userRoles) ? course.content : '',
+			videoSourceUrl: testUserRoles(course.roles as Role[], userRoles) ? course.videoSourceUrl : '',
+		}));
+
+		return {
+			status: 'SUCCESSFUL',
+			data: returnableCourses,
 		};
 	}
 }
