6657627b49a20e015c693d5cc371298c17bd1a09
Claus Haas <claus.haas@me.com>
2024-02-26 01:03:12 -0300

    add module router

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import {Router} from 'express';
 import courseRouter from './course.router';
 import userRouter from './user.router';
+import moduleRouter from './module.router';
 
 const router = Router();
 
 router.use('/courses', courseRouter);
+router.use('/modules', moduleRouter);
 router.use('/users', userRouter);
 
 export default router;
---
+++ backend/src/routes/module.router.ts
@@ -0,0 +1,5 @@
+import {Router} from 'express';
+
+const moduleRouter = Router();
+
+export default moduleRouter;
