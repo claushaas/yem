256a08407311346a48c3ed8c1fc2f839a92f4b38
Claus Haas <claus.haas@me.com>
2024-02-26 22:12:20 -0300

    add route post /modules

--- backend/src/routes/module.router.ts
+++ backend/src/routes/module.router.ts
@@ -1,5 +1,13 @@
 import {Router} from 'express';
+import ModuleController from '../controllers/module.controller';
 
 const moduleRouter = Router();
+
+moduleRouter.post(
+	'/',
+	async (req, res) => {
+		await new ModuleController().create(req, res);
+	},
+);
 
 export default moduleRouter;
