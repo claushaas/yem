37824c3904c8beaad14157aa15fee29830f7e5c6
Claus Haas <claus.haas@me.com>
2024-02-26 22:12:48 -0300

    add module controller with create method

---
+++ backend/src/controllers/module.controller.ts
@@ -0,0 +1,64 @@
+import {type Request, type Response} from 'express';
+import mapStatusHttp from '../utils/mapStatusHttp';
+import ModuleService from '../services/module.service';
+import {type TypeModule} from '../types/Module';
+
+export default class ModuleController {
+	private readonly _service: ModuleService;
+
+	constructor(service: ModuleService = new ModuleService()) {
+		this._service = service;
+	}
+
+	public async create(req: Request, res: Response) {
+		const moduleData = req.body as TypeModule;
+
+		const {status, data} = await this._service.create(moduleData);
+
+		const statusCode = mapStatusHttp(status);
+
+		return res.status(statusCode).json(data);
+	}
+
+	// Public async getAll(req: Request, res: Response) {
+	// 	const {roles: userRoles} = res.locals.user as TypeUser;
+
+	// 	const {status, data} = await this._service.getAll(userRoles);
+
+	// 	const statusCode = mapStatusHttp(status);
+
+	// 	return res.status(statusCode).json(data);
+	// }
+
+	// public async getById(req: Request, res: Response) {
+	// 	const {id} = req.params;
+	// 	const {roles: userRoles} = res.locals.user as TypeUser;
+
+	// 	const {status, data} = await this._service.getById(id, userRoles);
+
+	// 	const statusCode = mapStatusHttp(status);
+
+	// 	return res.status(statusCode).json(data);
+	// }
+
+	// public async update(req: Request, res: Response) {
+	// 	const {id} = req.params;
+	// 	const moduleData = req.body as TypeModule;
+
+	// 	const {status, data} = await this._service.update(id, moduleData);
+
+	// 	const statusCode = mapStatusHttp(status);
+
+	// 	return res.status(statusCode).json(data);
+	// }
+
+	// public async delete(req: Request, res: Response) {
+	// 	const {id} = req.params;
+
+	// 	const {status, data} = await this._service.delete(id);
+
+	// 	const statusCode = mapStatusHttp(status);
+
+	// 	return res.status(statusCode).json(data);
+	// }
+}
