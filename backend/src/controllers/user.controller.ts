c0120453d1d98164ba3a78a7d6b27fd4c72cb04b
Claus Haas <claus.haas@me.com>
2024-02-28 22:10:29 -0300

    refactor user controller

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,6 +1,8 @@
 import {type Request, type Response} from 'express';
 import UserService from '../services/user.service';
 import mapStatusHttp from '../utils/mapStatusHttp';
+import {type TypeHttpStatus} from '../types/HTTPStatus';
+import type TypeUser from '../types/User';
 
 export default class UserController {
 	private readonly _userService: UserService;
@@ -12,17 +14,17 @@ export default class UserController {
 	public async login(req: Request, res: Response) {
 		const {username, password} = req.body as {username: string; password: string};
 
-		const {status, data} = await this._userService.login(username, password);
+		const {status, data: {token, userData}} = await this._userService.login(username, password) as {status: TypeHttpStatus; data: {token: string; userData: TypeUser}};
 
 		const statusCode = mapStatusHttp(status);
 
 		return res.status(statusCode)
-			.cookie('access_token', data.token, {
+			.cookie('access_token', token, {
 				httpOnly: true,
 				secure: process.env.NODE_ENV === 'production',
 			})
 			.json({
-				userData: data.userData,
+				userData,
 			});
 	}
 
