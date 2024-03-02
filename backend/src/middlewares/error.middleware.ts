87e15e6dcdb4134691fb610b8024b08d40f3ebcf
Claus Haas <claus.haas@me.com>
2024-03-01 23:27:10 -0300

    refactor error middleware

--- backend/src/middlewares/error.middleware.ts
+++ backend/src/middlewares/error.middleware.ts
@@ -1,7 +1,7 @@
 import {type NextFunction, type Request, type Response} from 'express';
 import type TypeCustomError from '../types/CustomError';
 
-const errorMiddleware = (error: TypeCustomError, req: Request, res: Response, next: NextFunction) => {
+const errorMiddleware = (error: TypeCustomError, _req: Request, res: Response, _next: NextFunction) => {
 	const statusCode = error.statusCode ?? 500;
 	const message = error.message ?? 'Something went wrong';
 
