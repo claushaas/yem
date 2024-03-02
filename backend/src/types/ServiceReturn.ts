9a85b7fc9956cbfa3ff99a621cc62e2b16d561c8
Claus Haas <claus.haas@me.com>
2024-03-01 23:19:41 -0300

    refactor service return type

--- backend/src/types/ServiceReturn.ts
+++ backend/src/types/ServiceReturn.ts
@@ -1,3 +1,3 @@
 import {type TypeHttpStatus} from './HTTPStatus';
 
-export type TypeServiceReturn = {status: TypeHttpStatus; data: unknown};
+export type TypeServiceReturn<T> = {status: TypeHttpStatus; data: T};
