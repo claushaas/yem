80e522f513ade1454d87806e93f0fa053d4fefee
Claus Haas <claus.haas@me.com>
2024-03-02 03:15:39 -0300

    add types for hotmart and iugu subscriptions

--- backend/src/types/Subscription.ts
+++ backend/src/types/Subscription.ts
@@ -5,3 +5,17 @@ export type TypeSubscription = {
 	provider: 'hotmart' | 'iugu';
 	providerSubscriptionId: string;
 };
+
+export type TypeHotmartSubscription = {
+	subscription_id: string;
+	date_next_charge: string;
+	product: {
+		id: number;
+	};
+};
+
+export type TypeIuguSubscription = {
+	id: string;
+	plan_identifier: string;
+	expires_at: string;
+};
