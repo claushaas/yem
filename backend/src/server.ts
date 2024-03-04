77463b434f288821bb6bbd2d438a509ccdee0a18
Claus Haas <claus.haas@me.com>
2024-03-04 10:55:59 -0300

    remove newrelic instrumentation

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,7 @@
 import App from './app';
-import newrelic from 'newrelic';
 
 const port = process.env.APP_PORT ?? 3001;
 
 const app = new App();
 app.start(port);
 
-newrelic.instrumentLoadedModule('express', app);
