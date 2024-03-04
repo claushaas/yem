5324c9f7bf7ed707e66ae7f2822ef76817e7bfd4
Claus Haas <claus.haas@me.com>
2024-03-04 10:56:27 -0300

    add logger logs and fix get new access token method

--- backend/src/services/hotmart.service.ts
+++ backend/src/services/hotmart.service.ts
@@ -4,6 +4,8 @@ import {type TypeServiceReturn} from '../types/ServiceReturn';
 import {SecretService} from './secret.service';
 import type TypeUser from '../types/User';
 import {type TypeHotmartSubscription, type TypeSubscription} from '../types/Subscription';
+import {logger} from '../utils/Logger';
+import axios from 'axios';
 
 const baseUrl = process.env.HOTMART_API_URL ?? 'https://sandbox.hotmart.com/';
 
@@ -15,7 +17,11 @@ export class HotmartService {
 	}
 
 	public async getUserSubscriptions(user: TypeUser): Promise<TypeServiceReturn<TypeSubscription[]>> {
+		logger.logDebug('Getting user subscriptions');
+
 		const secrets = await this._secretService.getSecret();
+
+		logger.logDebug(`Got secrets: ${JSON.stringify(secrets)}`);
 
 		const request = new Request(baseUrl, {
 			'Content-Type': 'application/json',
@@ -31,15 +37,20 @@ export class HotmartService {
 
 		try {
 			const response = await request.get(url, params);
+			logger.logDebug(`Got response: ${JSON.stringify(response.data)}`);
 
 			return {
 				status: 'SUCCESSFUL',
 				data: response.data.items ? this._mapSubscriptions(response.data.items as TypeHotmartSubscription[], user) : [],
 			};
 		} catch (error) {
+			logger.logError(`Error getting user subscriptions on first try: ${JSON.stringify((error as Record<string, string>).data)}`);
 			try {
+				logger.logDebug('Getting new access token');
 				const newAccessToken = await this._getAccessToken();
+				logger.logDebug('Got new access token');
 
+				logger.logDebug('Trying to get user subscriptions again');
 				const request = new Request(baseUrl, {
 					'Content-Type': 'application/json',
 					// eslint-disable-next-line @typescript-eslint/naming-convention
@@ -53,6 +64,8 @@ export class HotmartService {
 					data: response.data.items ? this._mapSubscriptions(response.data.items as TypeHotmartSubscription[], user) : [],
 				};
 			} catch (error) {
+				console.log(error);
+				logger.logError(`Error getting user subscriptions on second try: ${JSON.stringify((error as Record<string, string>).data)}`);
 				throw new CustomError('INVALID_DATA', (error as Error).message);
 			}
 		}
@@ -70,31 +83,56 @@ export class HotmartService {
 
 	private async _getAccessToken(): Promise<string> {
 		try {
+			logger.logDebug('Getting new access token: _getAccessToken');
+			logger.logDebug('Getting secrets');
 			const {data: secrets} = await this._secretService.getSecret();
+			logger.logDebug('Got secrets');
 
-			const request = new Request('https://api-sec-vlc.hotmart.com', {
-				'Content-Type': 'application/json',
-				// eslint-disable-next-line @typescript-eslint/naming-convention
-				Authorization: `Basic ${secrets.hotmartApiClientBasic}`,
-			});
+			// Logger.logDebug('Creating new request for hotmart auth service');
+			// const request2 = new Request('https://api-sec-vlc.hotmart.com', {
+			// 	'Content-Type': 'application/json',
+			// 	// eslint-disable-next-line @typescript-eslint/naming-convention
+			// 	Authorization: secrets.hotmartApiClientBasic,
+			// });
 
-			const response = await request.post({
-				url: 'https://api-sec-vlc.hotmart.com/security/oauth/token?grant_type=client_credentials',
-				params: {
-					// eslint-disable-next-line @typescript-eslint/naming-convention
-					client_id: secrets.hotmartApiClientId,
-					// eslint-disable-next-line @typescript-eslint/naming-convention
-					client_secret: secrets.hotmartApiClientSecret,
+			logger.logDebug('Sending request for hotmart auth service');
+
+			const response = await axios.post(
+				'https://api-sec-vlc.hotmart.com/security/oauth/token',
+				null,
+				{
+					headers: {
+						'Content-Type': 'application/json',
+						// eslint-disable-next-line @typescript-eslint/naming-convention
+						Authorization: process.env.HOTMART_API_BASIC ?? '',
+					},
+					params: {
+						// eslint-disable-next-line @typescript-eslint/naming-convention
+						grant_type: 'client_credentials',
+						// eslint-disable-next-line @typescript-eslint/naming-convention
+						client_id: process.env.HOTMART_API_CLIENT_ID ?? '',
+						// eslint-disable-next-line @typescript-eslint/naming-convention
+						client_secret: process.env.HOTMART_API_SECRET ?? '',
+					},
 				},
-			});
+			);
 
+			// Const response = await request2.post({
+			// 	url: `https://api-sec-vlc.hotmart.com/security/oauth/token?grant_type=client_credentials&client_id=${secrets.hotmartApiClientId}&client_secret=${secrets.hotmartApiClientSecret}`,
+			// });
+			logger.logDebug('Got response');
+
+			logger.logDebug('Sending new hotmart access token to secret service');
 			await this._secretService.setSecret({
 				...secrets,
 				hotmartApiAccessToken: response.data.access_token as string,
 			});
+			logger.logDebug('Saved new hotmart access token to secret service');
 
 			return response.data.access_token as string;
 		} catch (error) {
+			console.log(error);
+			logger.logError(`Error getting new access token: ${JSON.stringify((error as Record<string, string>).data)}`);
 			throw new CustomError('INVALID_DATA', (error as Error).message);
 		}
 	}
