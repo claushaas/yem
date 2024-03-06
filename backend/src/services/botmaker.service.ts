764bb1e6f21ec78e274a5b4e5932587de9923d43
Claus Haas <claus.haas@me.com>
2024-03-06 16:40:05 -0300

    Revert "Add features already working on staging (#19)" (#20)
    This reverts commit 596e194f67296587a203ba43792435a03f514523.

--- backend/src/services/botmaker.service.ts
+++ backend/src/services/botmaker.service.ts
@@ -15,38 +15,6 @@ export class BotmakerService {
 		this._secretService = new SecretService();
 	}
 
-	public async addToBlackList(phoneNumber: string): Promise<TypeServiceReturn<AxiosResponse>> {
-		const {data: {botmakerApiAccessToken}} = await this._secretService.getSecret();
-
-		const request = new Request(baseUrl, {
-			'Content-Type': 'application/json',
-			// eslint-disable-next-line @typescript-eslint/naming-convention
-			Accept: 'application/json',
-			'access-token': botmakerApiAccessToken,
-		});
-
-		const url = '/notifications/contacts-blacklist';
-		const data = {
-			contacts: [phoneNumber],
-		};
-
-		try {
-			const response = await request.post(url, data);
-
-			return {
-				status: 'SUCCESSFUL',
-				data: response.data as AxiosResponse,
-			};
-		} catch (error) {
-			logger.logError(`Error adding to blacklist: ${JSON.stringify((error as {
-				response: {
-					data: any;
-				};
-			}).response.data)}`);
-			throw new CustomError('INVALID_DATA', `Error adding ${phoneNumber} to blacklist`);
-		}
-	}
-
 	public async sendWhatsappTemplateMessate(
 		userPhoneNumber: string,
 		whatsappTemplateName: string,
