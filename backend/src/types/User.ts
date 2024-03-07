e8ccf8ea77c1fca6cf447a71dc175ed95a84a446
Claus Haas <claus.haas@me.com>
2024-03-07 12:39:39 -0300

    add type for mautic user creation. attributes

--- backend/src/types/User.ts
+++ backend/src/types/User.ts
@@ -1,16 +1,24 @@
 import {type TypeUuid} from './UUID';
 
 export type UserRoles = string[];
+type Email = string;
+type FirstName = string;
+type LastName = string;
 
 type TypeUser = {
 	id: TypeUuid;
-	email: string;
+	email: Email;
 	roles?: UserRoles;
-	firstName: string;
-	lastName: string;
+	firstName: FirstName;
+	lastName: LastName;
 	phoneNumber: string;
 };
 
 export type TypeUserCreationAttributes = TypeUser & {document: string};
+export type TypeMauticUserCreationAttributes = {
+	email: Email;
+	firstName: FirstName;
+	lastName: LastName;
+};
 
 export default TypeUser;
