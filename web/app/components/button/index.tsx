389b2e43015047eece2c2fef2dbd0c30d47e80ee
Claus Haas <claus.haas@me.com>
2024-03-11 19:06:53 -0300

    refactor button

--- web/app/components/button/index.tsx
+++ web/app/components/button/index.tsx
@@ -1,8 +1,21 @@
 import React from 'react';
 
+export enum ButtonPreset {
+	Primary = 'primary',
+	Secondary = 'secondary',
+}
+
+enum ButtonType {
+	Button = 'button',
+	Submit = 'submit',
+	Reset = 'reset',
+}
+
 type ButtonProps = {
 	text?: string;
 	disabled?: boolean;
+	preset?: ButtonPreset;
+	type?: ButtonType;
 };
 
 const baseButtonClasses = `
@@ -28,11 +41,10 @@ const baseButtonClasses = `
 	disabled:shadow-none
 `;
 
-export const PrimaryButton = ({text = 'Clicar Aqui', disabled = false}: ButtonProps) => (
-	<>
-		<button className='shadow-purple-12'>Teste</button>
-		<button
-			className={baseButtonClasses + `
+const classNames = (preset: ButtonPreset) => {
+	switch (preset) {
+		case ButtonPreset.Primary:
+			return baseButtonClasses + `
 				bg-purple-9
 				dark:bg-purpledark-9
 				text-whitea-11
@@ -42,29 +54,35 @@ export const PrimaryButton = ({text = 'Clicar Aqui', disabled = false}: ButtonPr
 				active:dark:bg-purpledark-12
 				focus:ring-purple-9
 				shadow-purple-12
-			`}
+			`;
+		case ButtonPreset.Secondary:
+			return baseButtonClasses + `
+				bg-purple-3
+				dark:bg-purpledark-3
+				text-blacka-7
+				dark:text-whitea-7
+				hover:bg-purple-4
+				hover:dark:bg-purpledark-4
+				active:bg-purple-5
+				active:dark:bg-purpledark-5
+				focus:ring-purple-3
+			`;
+	}
+};
+
+export const Button = ({
+	disabled = false,
+	preset = ButtonPreset.Primary,
+	text = 'Clicar Aqui',
+	type = ButtonType.Button,
+}: ButtonProps) => (
+	<>
+		<button
+			className={classNames(preset)}
 			disabled={disabled}
+			type={type}
 		>
 			{text}
 		</button>
 	</>
-);
-
-export const SecondaryButton = ({text = 'Clicar Aqui', disabled = false}: ButtonProps) => (
-	<button
-		className={baseButtonClasses + `
-			bg-purple-3
-			dark:bg-purpledark-3
-			text-blacka-7
-			dark:text-whitea-7
-			hover:bg-purple-4
-			hover:dark:bg-purpledark-4
-			active:bg-purple-5
-			active:dark:bg-purpledark-5
-			focus:ring-purple-3
-		`}
-		disabled={disabled}
-	>
-		{text}
-	</button>
 );
