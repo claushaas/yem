add6d22b27c7602116f0785a7d303d3391c33f90
Claus Haas <claus.haas@me.com>
2024-03-11 21:42:20 -0300

    ad dui page and component for logo

--- web/app/routes/ui.button.tsx
+++ web/app/routes/ui.button.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import {Button, ButtonPreset} from '~/components/button';
 
-export default function ButtonUi() {
-	return (
-		<div>
-			<h1>Buttons</h1>
-			<h2>Primary Button</h2>
-			<Button preset={ButtonPreset.Primary}/>
-			<h2>Disabled Button</h2>
-			<Button disabled />
-			<h2>Secondary Button</h2>
-			<Button preset={ButtonPreset.Secondary}/>
-		</div>
-	);
-}
+const ButtonUi = () => (
+	<>
+		<h1>Buttons</h1>
+		<h2>Primary Button</h2>
+		<Button preset={ButtonPreset.Primary}/>
+		<h2>Disabled Button</h2>
+		<Button disabled />
+		<h2>Secondary Button</h2>
+		<Button preset={ButtonPreset.Secondary}/>
+	</>
+);
+
+export default ButtonUi;
--- web/app/routes/ui.tsx
+++ web/app/routes/ui.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import {Link, Outlet} from '@remix-run/react';
 
-export default function Ui() {
-	return (
-		<div>
-			<h1>UI</h1>
-			<Link to='/ui/button'>Button</Link>
-			<Link to='/ui/container'>Container</Link>
-			<Outlet />
-		</div>
-	);
-}
+const Ui = () => (
+	<div>
+		<h1>UI</h1>
+		<Link to='/ui/button'>Button</Link>
+		<Link to='/ui/container'>Container</Link>
+		<Link to='/ui/logo'>Logo</Link>
+		<Outlet />
+	</div>
+);
+export default Ui;
