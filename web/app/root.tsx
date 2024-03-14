20a103941f92fa555268ae4ed87ac97dee249967
Claus Haas <claus.haas@me.com>
2024-03-14 18:09:35 -0300

    add route change animations

--- web/app/root.tsx
+++ web/app/root.tsx
@@ -6,11 +6,13 @@ import {
 	Scripts,
 	ScrollRestoration,
 } from '@remix-run/react';
-import React from 'react';
+import React, {useState} from 'react';
 import {type LoaderFunctionArgs, type LinksFunction, json} from '@remix-run/node';
 import styles from '~/tailwind.css?url';
 import {NavBar} from '~/components/navBar';
 import {getUserSession} from './utils/session.server';
+import {AnimatePresence, motion} from 'framer-motion';
+import {useLocation, useOutlet} from 'react-router-dom';
 
 export const links: LinksFunction = () => [
 	{rel: 'stylesheet', href: styles},
@@ -35,23 +37,36 @@ export const loader = async ({request}: LoaderFunctionArgs) => {
 	return json({userData: null});
 };
 
-export const Layout = ({children}: {children: React.ReactNode}) => (
-	<html lang='pt-BR' className='notranslate' translate='no'>
-		<head>
-			<meta charSet='utf-8' />
-			<meta name='viewport' content='width=device-width,initial-scale=1,viewport-fit=cover' />
-			<Meta />
-			<Links />
-		</head>
-		<body className='bg-mauve-2 dark:bg-mauvedark-2 min-h-screen flex flex-col'>
-			<NavBar />
-			{children}
-			<ScrollRestoration />
-			<Scripts />
-		</body>
-	</html>
-);
+function AnimatedOutlet() {
+	const [outlet] = useState(useOutlet());
+	return outlet;
+}
 
 export default function App() {
-	return <Outlet />;
+	return (
+		<html lang='pt-BR' className='notranslate' translate='no'>
+			<head>
+				<meta charSet='utf-8' />
+				<meta name='viewport' content='width=device-width,initial-scale=1,viewport-fit=cover' />
+				<Meta />
+				<Links />
+			</head>
+			<body className='bg-mauve-2 dark:bg-mauvedark-2 min-h-screen flex flex-col'>
+				<NavBar />
+				<AnimatePresence mode='wait' initial={false}>
+					<motion.main
+						key={useLocation().pathname}
+						initial={{opacity: 0}}
+						animate={{opacity: 1}}
+						exit={{opacity: 0}}
+						transition={{duration: 0.3}}
+					>
+						<AnimatedOutlet />
+					</motion.main>
+				</AnimatePresence>
+				<ScrollRestoration />
+				<Scripts />
+			</body>
+		</html>
+	);
 }
