914551260cb8beb0310045ab278f21775ac41fd4
Claus Haas <claus.haas@me.com>
2024-03-20 17:08:42 -0300

    refactor

--- api/utils/remixServerUtils.js
+++ api/utils/remixServerUtils.js
@@ -1,7 +1,17 @@
 import {createRequestHandler} from '@remix-run/express';
 import process from 'process';
 
-export const viteDevServer = process.env.NODE_ENV === 'production' ? undefined : await import('vite').then(vite => vite.createServer({server: {middlewareMode: true}}));
+export const viteDevServer = process.env.NODE_ENV === 'production' ? undefined : await import('vite').then(vite => vite.createServer({
+	server: {
+		middlewareMode: true,
+		watch: {
+			ignored: ['!**/node_modules/@newrelic/**'],
+		},
+	},
+	optimizeDeps: {
+		exclude: ['newrelic', '@newrelic'],
+	},
+}));
 
 export const remixHandler = createRequestHandler({
 	build: viteDevServer
