5248c68a7f8f994b814f81b6d49d224164148cfe
Claus Haas <claus.haas@me.com>
2024-03-20 15:25:42 -0300

    fix cookie settings for development

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -6,11 +6,11 @@ export const {
 	destroySession: destroyUserSession,
 } = createCookieSessionStorage({
 	cookie: {
-		httpOnly: true,
+		httpOnly: process.env.NODE_ENV === 'production',
 		secure: process.env.NODE_ENV === 'production',
 		name: '__session',
 		maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
 		secrets: [process.env.YEM_REMIX_COOKIE_SESSION_SECRET!],
-		sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none',
+		sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'lax',
 	},
 });
