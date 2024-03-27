2fd159d9a13e19455571d7dafa23b143e800a8c5
Claus Haas <claus.haas@me.com>
2024-03-27 11:12:34 -0300

    add module data

--- app/routes/admin.courses.$course-id.tsx
+++ app/routes/admin.courses.$course-id.tsx
@@ -23,19 +23,19 @@ export const loader = async ({request, params}: LoaderFunctionArgs) => {
 export default function Course() {
 	const {course} = useLoaderData<{course: TPrismaPayloadGetCourseById | undefined}>();
 
-	return (
+	return course && (
 		<>
 			<div>
-				<h1>{course?.name}</h1>
-				<h2>{course?.description}</h2>
-				<p>Data de publicação: {course?.publicationDate}</p>
-				<p>Está publicado: {course?.published ? 'sim' : 'não'}</p>
+				<h1>{course.name}</h1>
+				<h2>{course.description}</h2>
+				<p>Data de publicação: {course.publicationDate}</p>
+				<p>Está publicado: {course.published ? 'sim' : 'não'}</p>
 				<p>Está com matrículas abertas: {course?.isSelling ? 'sim' : 'não'}</p>
 			</div>
 			<div>
 				<h3>Módulos</h3>
 				<div>
-					{course?.modules.map(module => (
+					{course.modules.map(module => (
 						<CourseCard key={module.id} course={module} to={`./${module.id}`}/>
 					))}
 				</div>
--- app/routes/admin.courses.$course-id_.$module-id.tsx
+++ app/routes/admin.courses.$course-id_.$module-id.tsx
@@ -1,7 +1,47 @@
+import {json, type LoaderFunctionArgs} from '@remix-run/node';
+import {useLoaderData} from '@remix-run/react';
+import {logger} from '#/utils/logger.util';
+import {getUserSession} from '~/utils/session.server';
+import {ModuleService} from '#/services/module.service';
+import {type TUser} from '#/types/user.type';
+import {type TPrismaPayloadGetModuleById} from '#/types/module.type';
+import {CourseCard} from '~/components/course-card/index.js';
+
+export const loader = async ({request, params}: LoaderFunctionArgs) => {
+	const {data: userData} = await getUserSession(request.headers.get('Cookie'));
+	const {
+		'course-id': courseId,
+		'module-id': moduleId,
+	} = params;
+
+	try {
+		const {data: module} = await new ModuleService().getById(courseId!, moduleId!, userData as TUser);
+		return json({module});
+	} catch (error) {
+		logger.logError(`Error getting module: ${(error as Error).message}`);
+		return null;
+	}
+};
+
 export default function Module() {
-	return (
-		<div>
-			<h1>Module</h1>
-		</div>
+	const {module} = useLoaderData<{module: TPrismaPayloadGetModuleById | undefined}>();
+
+	return module && (
+		<>
+			<div>
+				<h1>{module.name}</h1>
+				<h2>{module.description}</h2>
+				<p>Data de publicação: {module.publicationDate}</p>
+				<p>Está publicado: {module.published ? 'sim' : 'não'}</p>
+			</div>
+			<div>
+				<h3>Aulas</h3>
+				<div>
+					{module.lessons.map(lesson => (
+						<CourseCard key={lesson.id} course={lesson} to={`./${lesson.id}`}/>
+					))}
+				</div>
+			</div>
+		</>
 	);
 }
