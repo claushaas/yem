eeed23eb708a8210cdbc24bcbe1ada4123996188
Claus Haas <claus.haas@me.com>
2024-03-26 22:42:13 -0300

    add types for data returns of course service

--- api/types/course.type.ts
+++ api/types/course.type.ts
@@ -1,3 +1,4 @@
+import {type Prisma} from '@prisma/client';
 import {type TTags} from './tag.type.js';
 
 export type TCourse = {
@@ -10,3 +11,66 @@ export type TCourse = {
 	published: boolean;
 	tags?: TTags;
 };
+
+export type TPrismaPayloadGetAllCourses = Array<Prisma.CourseGetPayload<{
+	select: {
+		id: true;
+		name: true;
+		description: true;
+		thumbnailUrl: true;
+		published: true;
+		publicationDate: true;
+	};
+}>>;
+
+export type TPrismaPayloadGetCourseById = Prisma.CourseGetPayload<{
+	include: {
+		modules: {
+			select: {
+				id: true;
+				name: true;
+				description: true;
+				thumbnailUrl: true;
+				published: true;
+				publicationDate: true;
+			};
+		};
+		comments: {
+			select: {
+				id: true;
+				content: true;
+				createdAt: true;
+				userId: true;
+				responses: {
+					select: {
+						id: true;
+						content: true;
+						createdAt: true;
+						userId: true;
+					};
+				};
+			};
+		};
+	};
+}>;
+
+export type TPrismaPayloadCreateCourse = Prisma.CourseGetPayload<{
+	include: {
+		tags: {
+			include: {
+				tagOption: {
+					select: {
+						name: true;
+					};
+				};
+				tagValue: {
+					select: {
+						name: true;
+					};
+				};
+			};
+		};
+	};
+}>;
+
+export type TPrismaPayloadUpdateCourse = TPrismaPayloadCreateCourse;
