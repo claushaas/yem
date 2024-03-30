926af2c8130c8aefc5cfa42b563ec3dd8f553be9
Claus Haas <claus.haas@me.com>
2024-03-29 22:13:09 -0300

    fixes in editor component

--- app/routes/editor.tsx
+++ app/routes/editor.tsx
@@ -1,11 +1,23 @@
-import {Suspense} from 'react';
-import TextEditor from '~/components/text-editor/index.client.js';
+import type Quill from 'quill';
+import {useEffect, useState} from 'react';
+import {ClientOnly} from 'remix-utils/client-only';
+import {Editor} from '~/components/text-editor/index.client.js';
 import {YemSpinner} from '~/components/yem-spinner/index.js';
 
-export default function Editor() {
+export default function TextEditor() {
+	const [quill, setQuill] = useState<Quill | null>(null); // eslint-disable-line @typescript-eslint/ban-types
+
+	useEffect(() => {
+		if (quill) {
+			quill.on('text-change', () => {
+				console.log('text-change', quill.getSemanticHTML());
+			});
+		}
+	}, [quill]);
+
 	return (
-		<Suspense fallback={<YemSpinner/>}>
-			<TextEditor/>
-		</Suspense>
+		<ClientOnly fallback={<YemSpinner/>}>
+			{() => <Editor setQuill={setQuill}/>}
+		</ClientOnly>
 	);
 }
