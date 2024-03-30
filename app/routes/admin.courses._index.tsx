c9255f10f7f9965332303f66f58c40bc4fad1d9a
Claus Haas <claus.haas@me.com>
2024-03-30 14:46:01 -0300

    fix typing

--- app/routes/admin.courses._index.tsx
+++ app/routes/admin.courses._index.tsx
@@ -1,26 +1,39 @@
 import {XMarkIcon} from '@heroicons/react/24/outline';
 import * as Dialog from '@radix-ui/react-dialog';
-import {type ActionFunctionArgs, json, type LoaderFunctionArgs} from '@remix-run/node';
+import {
+	type ActionFunctionArgs, json, type LoaderFunctionArgs, redirect,
+} from '@remix-run/node';
 import {Form, useLoaderData, useNavigation} from '@remix-run/react';
 import * as RadixForm from '@radix-ui/react-form';
 import type Quill from 'quill';
 import {useEffect, useState} from 'react';
 import {ClientOnly} from 'remix-utils/client-only';
+import * as Switch from '@radix-ui/react-switch';
 import {CourseService} from '#/services/course.service';
 import {Button, ButtonPreset, ButtonType} from '~/components/button/index.js';
-import {getUserSession} from '~/utils/session.server.js';
+import {commitUserSession, getUserSession} from '~/utils/session.server.js';
 import {logger} from '#/utils/logger.util.js';
 import {CourseCard} from '~/components/course-card/index.js';
 import {Editor} from '~/components/text-editor/index.client.js';
 import {YemSpinner} from '~/components/yem-spinner/index.js';
+import {type TUserRoles} from '#/types/user.type';
+import {type TPrismaPayloadGetAllCourses} from '#/types/course.type';
+
+type CoursesLoaderData = {
+	error: string | undefined;
+	success: string | undefined;
+	courses: TPrismaPayloadGetAllCourses;
+};
 
 export const loader = async ({request}: LoaderFunctionArgs) => {
-	const {data: userData} = await getUserSession(request.headers.get('Cookie'));
+	const userSession = await getUserSession(request.headers.get('Cookie'));
 
 	try {
-		const response = await new CourseService().getAll(userData.roles as string[] ?? []);
-		return json({
-			courses: response.data,
+		const {data: courses} = await new CourseService().getAll(userSession.get('roles') as TUserRoles);
+		return json<CoursesLoaderData>({
+			error: userSession.get('error') as string | undefined,
+			success: userSession.get('success') as string | undefined,
+			courses,
 		});
 	} catch (error) {
 		logger.logDebug(`Error getting courses: ${(error as Error).message}`);
@@ -31,32 +44,57 @@ export const loader = async ({request}: LoaderFunctionArgs) => {
 export const action = async ({request}: ActionFunctionArgs) => {
 	const userSession = await getUserSession(request.headers.get('Cookie'));
 
-	if ((userSession.get('roles') as string[])?.includes('admin')) {
-		const formData = await request.formData();
-		const name = formData.get('name');
-		const description = formData.get('description');
-		const content = formData.get('content');
-		const videoSourceUrl = formData.get('videoSourceUrl');
-		const thumbnailUrl = formData.get('thumbnailUrl');
-		const publicationDate = formData.get('publicationDate');
-		const published = formData.get('published');
-		const isSelling = formData.get('isSelling');
+	try {
+		if ((userSession.get('roles') as string[])?.includes('admin')) {
+			const formData = await request.formData();
 
-		console.log('name', name);
-		console.log('description', description);
-		console.log('content', content);
-		console.log('videoSourceUrl', videoSourceUrl);
-		console.log('thumbnailUrl', thumbnailUrl);
-		console.log('publicationDate', publicationDate);
-		console.log('published', published);
-		console.log('isSelling', isSelling);
+			const newCourse = {
+				name: formData.get('name') as string,
+				description: formData.get('description') as string,
+				content: formData.get('content') as string,
+				videoSourceUrl: formData.get('videoSourceUrl') as string,
+				thumbnailUrl: formData.get('thumbnailUrl') as string,
+				publicationDate: new Date(formData.get('publicationDate') as string),
+				published: Boolean(formData.get('published')),
+				isSelling: Boolean(formData.get('isSelling')),
+			};
+
+			console.log(newCourse);
+
+			await new CourseService().create(newCourse);
+
+			userSession.flash('success', `Curso ${newCourse.name} criado com sucesso`);
+
+			return redirect('/admin/courses', {
+				headers: {
+					'Set-Cookie': await commitUserSession(userSession), // eslint-disable-line @typescript-eslint/naming-convention
+				},
+			});
+		}
+
+		userSession.flash('error', 'Você não tem permissão para criar cursos');
+		return redirect('/admin/courses', {
+			headers: {
+				'Set-Cookie': await commitUserSession(userSession), // eslint-disable-line @typescript-eslint/naming-convention
+			},
+		});
+	} catch {
+		logger.logError('Error creating course');
+		userSession.flash('error', 'Erro ao criar curso');
+		return redirect('/admin/courses', {
+			headers: {
+				'Set-Cookie': await commitUserSession(userSession), // eslint-disable-line @typescript-eslint/naming-convention
+			},
+		});
 	}
-
-	return null;
 };
 
 export default function Courses() {
-	const {courses} = useLoaderData<typeof loader>();
+	const {
+		courses,
+		error,
+		success,
+	} = useLoaderData<CoursesLoaderData>();
 	const [quill, setQuill] = useState<Quill | null>(null); // eslint-disable-line @typescript-eslint/ban-types
 	const [content, setContent] = useState<string>('');
 	const navigation = useNavigation();
@@ -72,6 +110,11 @@ export default function Courses() {
 
 	return (
 		<>
+			{(success ?? error) && (
+				<p className='mb-4 text-lg'>
+					{success ?? error}
+				</p>
+			)}
 			<Dialog.Root>
 				<Dialog.Trigger asChild>
 					<div className='w-fit'>
@@ -106,6 +149,7 @@ export default function Courses() {
 											<RadixForm.Control asChild>
 												<input
 													required
+													disabled={isSubmitting}
 													type='text'
 													min={8}
 													className='w-full bg-mauve-5 dark:bg-mauvedark-5 text-mauve-12 dark:text-mauvedark-11 inline-flex h-[35px] appearance-none items-center justify-center rounded-md px-[10px] text-[15px] leading-none outline-none'
@@ -122,6 +166,7 @@ export default function Courses() {
 											<RadixForm.Control asChild>
 												<input
 													required
+													disabled={isSubmitting}
 													type='text'
 													min={8}
 													className='w-full bg-mauve-5 dark:bg-mauvedark-5 text-mauve-12 dark:text-mauvedark-11 inline-flex h-[35px] appearance-none items-center justify-center rounded-md px-[10px] text-[15px] leading-none outline-none'
@@ -137,6 +182,7 @@ export default function Courses() {
 											</div>
 											<RadixForm.Control asChild>
 												<input
+													disabled={isSubmitting}
 													type='text'
 													min={8}
 													className='hidden'
@@ -157,6 +203,7 @@ export default function Courses() {
 											</div>
 											<RadixForm.Control asChild>
 												<input
+													disabled={isSubmitting}
 													type='text'
 													min={3}
 													className='w-full bg-mauve-5 dark:bg-mauvedark-5 text-mauve-12 dark:text-mauvedark-11 inline-flex h-[35px] appearance-none items-center justify-center rounded-md px-[10px] text-[15px] leading-none outline-none'
@@ -172,6 +219,7 @@ export default function Courses() {
 											</div>
 											<RadixForm.Control asChild>
 												<input
+													disabled={isSubmitting}
 													type='text'
 													min={3}
 													className='w-full bg-mauve-5 dark:bg-mauvedark-5 text-mauve-12 dark:text-mauvedark-11 inline-flex h-[35px] appearance-none items-center justify-center rounded-md px-[10px] text-[15px] leading-none outline-none'
@@ -187,6 +235,7 @@ export default function Courses() {
 											</div>
 											<RadixForm.Control asChild>
 												<input
+													disabled={isSubmitting}
 													type='datetime-local'
 													min={3}
 													className='w-full bg-mauve-5 dark:bg-mauvedark-5 text-mauve-12 dark:text-mauvedark-11 inline-flex h-[35px] appearance-none items-center justify-center rounded-md px-[10px] text-[15px] leading-none outline-none'
@@ -201,11 +250,14 @@ export default function Courses() {
 												</RadixForm.Label>
 											</div>
 											<RadixForm.Control asChild>
-												<input
-													type='text'
-													min={3}
-													className='w-full bg-mauve-5 dark:bg-mauvedark-5 text-mauve-12 dark:text-mauvedark-11 inline-flex h-[35px] appearance-none items-center justify-center rounded-md px-[10px] text-[15px] leading-none outline-none'
-												/>
+												<Switch.Root
+													disabled={isSubmitting}
+													className='w-[42px] h-[25px] bg-blacka-6 rounded-full relative shadow-[0_2px_10px] shadow-blacka-4 focus:shadow-[0_0_0_2px] focus:shadow-black data-[state=checked]:bg-black outline-none cursor-default'
+												>
+													<Switch.Thumb
+														className='block w-[21px] h-[21px] bg-white rounded-full shadow-[0_2px_2px] shadow-blackA4 transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]'
+													/>
+												</Switch.Root>
 											</RadixForm.Control>
 										</RadixForm.Field>
 
@@ -216,16 +268,19 @@ export default function Courses() {
 												</RadixForm.Label>
 											</div>
 											<RadixForm.Control asChild>
-												<input
-													type='text'
-													min={3}
-													className='w-full bg-mauve-5 dark:bg-mauvedark-5 text-mauve-12 dark:text-mauvedark-11 inline-flex h-[35px] appearance-none items-center justify-center rounded-md px-[10px] text-[15px] leading-none outline-none'
-												/>
+												<Switch.Root
+													disabled={isSubmitting}
+													className='w-[42px] h-[25px] bg-blacka-6 rounded-full relative shadow-[0_2px_10px] shadow-blacka-4 focus:shadow-[0_0_0_2px] focus:shadow-black data-[state=checked]:bg-black outline-none cursor-default'
+												>
+													<Switch.Thumb
+														className='block w-[21px] h-[21px] bg-white rounded-full shadow-[0_2px_2px] shadow-blackA4 transition-transform duration-100 translate-x-0.5 will-change-transform data-[state=checked]:translate-x-[19px]'
+													/>
+												</Switch.Root>
 											</RadixForm.Control>
 										</RadixForm.Field>
 
 										<RadixForm.Submit asChild>
-											<Button isDisabled={isSubmitting} className='m-auto mt-2' text='Criar Minha Conta' preset={ButtonPreset.Primary} type={ButtonType.Submit}/>
+											<Button isDisabled={isSubmitting} className='m-auto mt-2' text='Criar Curso' preset={ButtonPreset.Primary} type={ButtonType.Submit}/>
 										</RadixForm.Submit>
 
 										{isSubmitting && <YemSpinner/>}
@@ -249,7 +304,7 @@ export default function Courses() {
 			</Dialog.Root>
 			<div className='flex gap-4 my-4 flex-wrap'>
 				{courses?.map(course => (
-					<CourseCard key={course.id} course={course} to={`./${course.id}`}/>
+					<CourseCard key={course?.id} course={course ?? {}} to={`./${course?.id}`}/>
 				))}
 			</div>
 		</>
