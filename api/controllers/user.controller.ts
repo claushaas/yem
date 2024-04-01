0548c9645ecc84501526f614bb3d7b7dde527ae2
Claus Haas <claus.haas@me.com>
2024-04-01 11:46:37 -0300

    remove express user routes

--- api/controllers/user.controller.ts
+++ api/controllers/user.controller.ts
@@ -1,9 +1,9 @@
-import {type Request, type Response} from 'express';
+// Import {type Request, type Response} from 'express';
 import {UserService} from '../services/user.service.js';
-import {mapStatusHttp} from '../utils/map-status-http.js';
-import {type THttpStatus} from '../types/http-status.type.js';
-import {type TUser, type TUserCreationAttributes} from '../types/user.type.js';
-import {logger} from '../utils/logger.util.js';
+// Import {mapStatusHttp} from '../utils/map-status-http.js';
+// import {type THttpStatus} from '../types/http-status.type.js';
+// import {type TUser, type TUserCreationAttributes} from '../types/user.type.js';
+// import {logger} from '../utils/logger.util.js';
 
 export class UserController {
 	private readonly _userService: UserService;
@@ -12,53 +12,53 @@ export class UserController {
 		this._userService = service;
 	}
 
-	public async login(request: Request, response: Response) {
-		const {username, password} = request.body as {username: string; password: string};
+	// Public async login(request: Request, response: Response) {
+	// 	const {username, password} = request.body as {username: string; password: string};
 
-		const {status, data: {token, userData}} = await this._userService.login(username, password) as {status: THttpStatus; data: {token: string; userData: TUser}};
+	// 	const {status, data: {token, userData}} = await this._userService.login(username, password) as {status: THttpStatus; data: {token: string; userData: TUser}};
 
-		const statusCode = mapStatusHttp(status);
+	// 	const statusCode = mapStatusHttp(status);
 
-		return response.status(statusCode)
-			.cookie('access_token', token, {
-				httpOnly: process.env.NODE_ENV === 'production',
-				secure: process.env.NODE_ENV === 'production',
-				maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
-				sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none',
-			})
-			.json({
-				userData,
-			});
-	}
+	// 	return response.status(statusCode)
+	// 		.cookie('access_token', token, {
+	// 			httpOnly: process.env.NODE_ENV === 'production',
+	// 			secure: process.env.NODE_ENV === 'production',
+	// 			maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
+	// 			sameSite: process.env.NODE_ENV === 'production' ? 'strict' : 'none',
+	// 		})
+	// 		.json({
+	// 			userData,
+	// 		});
+	// }
 
-	public logout(_request: Request, response: Response) {
-		const statusCode = mapStatusHttp('SUCCESSFUL');
+	// public logout(_request: Request, response: Response) {
+	// 	const statusCode = mapStatusHttp('SUCCESSFUL');
 
-		logger.logInfo('User logged out successfully');
+	// 	logger.logInfo('User logged out successfully');
 
-		return response
-			.status(statusCode)
-			.clearCookie('access_token')
-			.json({message: 'User logged out successfully'});
-	}
+	// 	return response
+	// 		.status(statusCode)
+	// 		.clearCookie('access_token')
+	// 		.json({message: 'User logged out successfully'});
+	// }
 
-	public async createOrFail(request: Request, response: Response) {
-		const user = request.body as TUserCreationAttributes;
+	// public async createOrFail(request: Request, response: Response) {
+	// 	const user = request.body as TUserCreationAttributes;
 
-		const {status, data} = await this._userService.createOrFail(user);
+	// 	const {status, data} = await this._userService.createOrFail(user);
 
-		const statusCode = mapStatusHttp(status);
+	// 	const statusCode = mapStatusHttp(status);
 
-		return response.status(statusCode).json(data);
-	}
+	// 	return response.status(statusCode).json(data);
+	// }
 
-	public async getNewPassword(request: Request, response: Response) {
-		const {email} = request.body as {email: string};
+	// public async getNewPassword(request: Request, response: Response) {
+	// 	const {email} = request.body as {email: string};
 
-		const {status, data} = await this._userService.getNewPassword(email);
+	// 	const {status, data} = await this._userService.getNewPassword(email);
 
-		const statusCode = mapStatusHttp(status);
+	// 	const statusCode = mapStatusHttp(status);
 
-		return response.status(statusCode).json(data);
-	}
+	// 	return response.status(statusCode).json(data);
+	// }
 }
--- api/routes/user.router.ts
+++ api/routes/user.router.ts
@@ -1,32 +1,36 @@
-import {type Request, type Response, Router} from 'express';
-import {UserController} from '../controllers/user.controller.js';
+import {
+	// Type Request,
+	// type Response,
+	Router,
+} from 'express';
+// Import {UserController} from '../controllers/user.controller.js';
 
 // eslint-disable-next-line new-cap
 const userRouter = Router();
 
-userRouter.post(
-	'/login',
-	async (request: Request, response: Response) => {
-		await new UserController().login(request, response);
-	},
-);
+// UserRouter.post(
+// 	'/login',
+// 	async (request: Request, response: Response) => {
+// 		await new UserController().login(request, response);
+// 	},
+// );
 
-userRouter.get(
-	'/logout',
-	(request: Request, response: Response) => new UserController().logout(request, response),
-);
+// userRouter.get(
+// 	'/logout',
+// 	(request: Request, response: Response) => new UserController().logout(request, response),
+// );
 
-userRouter.post(
-	'/new-password',
-	async (request: Request, response: Response) => new UserController().getNewPassword(request, response),
-);
+// userRouter.post(
+// 	'/new-password',
+// 	async (request: Request, response: Response) => new UserController().getNewPassword(request, response),
+// );
 
-userRouter.post(
-	'/create-or-fail',
-	async (request: Request, response: Response) => {
-		await new UserController().createOrFail(request, response);
-	},
-);
+// userRouter.post(
+// 	'/create-or-fail',
+// 	async (request: Request, response: Response) => {
+// 		await new UserController().createOrFail(request, response);
+// 	},
+// );
 
 export {userRouter};
 
