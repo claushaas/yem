51102a6712af6cfe2bdcc599e92549403066a230
Claus Haas <claus.haas@me.com>
2024-04-05 20:56:22 -0300

    use hook service to handle iugu webhook calls

--- app/routes/api.iugu.tsx
+++ app/routes/api.iugu.tsx
@@ -1,25 +1,33 @@
-import {json, type LoaderFunctionArgs} from '@remix-run/node';
-import {SlackService} from '~/services/slack.service';
+import {type ActionFunctionArgs, json, type LoaderFunctionArgs} from '@remix-run/node';
+import {parse} from 'qs';
+import {HooksService} from '~/services/hooks.service';
+import {IuguService} from '~/services/iugu.service';
 import {logger} from '~/utils/logger.util';
 
 export const loader = async ({request}: LoaderFunctionArgs) => {
 	try {
-		await new SlackService().sendMessage(request.body as Record<string, any>);
-
 		return json({
 			message: 'OK',
 		}, {
 			status: 200,
 		});
 	} catch (error) {
-		logger.logError(`Error sending message: ${(error as Error).message}`);
+		logger.logError(`Error sending message on iugu loader: ${(error as Error).message}`);
 		return json({error: 'An error occurred'});
 	}
 };
 
-export const action = async ({request}: LoaderFunctionArgs) => {
+export const action = async ({request}: ActionFunctionArgs) => {
 	try {
-		await new SlackService().sendMessage(request.body);
+		const bodyText = await request.text();
+		const body = parse(bodyText) as {
+			event: string;
+			data: Record<string, any>;
+		};
+
+		console.log('body', body);
+
+		await new HooksService().handleIuguWebhook(body);
 
 		return json({
 			message: 'OK',
@@ -27,7 +35,7 @@ export const action = async ({request}: LoaderFunctionArgs) => {
 			status: 200,
 		});
 	} catch (error) {
-		logger.logError(`Error sending message: ${(error as Error).message}`);
-		return json({error: 'An error occurred'});
+		logger.logError(`Error sending message on iugu action: ${(error as Error).message}`);
+		return json({error: 'An error occurred'}, {status: 200});
 	}
 };
