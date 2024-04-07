a9692a942541dfcc56f78f3229d2b66329f5dbf3
Claus <claus.haas@me.com>
2024-04-07 20:40:00 -0300

    lint

--- app/cache/memory-cache.ts
+++ app/cache/memory-cache.ts
@@ -1,33 +1,34 @@
+// eslint-disable-next-line @typescript-eslint/no-extraneous-class
 export class MemoryCache {
-    private static cache = {} as Record<string, string>
-    
-    static get(key:string){
-        const value = MemoryCache.cache[key];
-        
-        if (value === undefined){
-            return null
-        }
-        
-        return value;
-    }
-    
-    static set(key: string, value: string){
-        if (!value || value === ''){
-            return
-        }
-        
-        MemoryCache.cache[key] = value;
-    }
-    
-    static del(key: string){
-        if (MemoryCache.cache[key] === undefined){
-            return
-        }
-        
-        delete MemoryCache.cache[key];
-    }
-    
-    static keys(){
-        return Object.keys(MemoryCache.cache);
-    }
+	static get(key: string) {
+		const value = MemoryCache.cache[key];
+
+		if (value === undefined) {
+			return null;
+		}
+
+		return value;
+	}
+
+	static set(key: string, value: string) {
+		if (!value || value === '') {
+			return;
+		}
+
+		MemoryCache.cache[key] = value;
+	}
+
+	static del(key: string) {
+		if (MemoryCache.cache[key] === undefined) {
+			return;
+		}
+
+		delete MemoryCache.cache[key]; // eslint-disable-line @typescript-eslint/no-dynamic-delete
+	}
+
+	static keys() {
+		return Object.keys(MemoryCache.cache);
+	}
+
+	private static cache: Record<string, string>;
 }
