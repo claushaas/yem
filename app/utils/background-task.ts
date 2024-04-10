c969895c630cab777722b90d6d014f4386616bce
Claus Haas <claus.haas@me.com>
2024-04-10 10:35:59 -0300

    lint

--- app/utils/background-task.ts
+++ app/utils/background-task.ts
@@ -1,16 +1,16 @@
 export async function executeAndRepeat(targetPromise: () => Promise<any>, milliseconds: number) {
-    if (process.env.NODE_ENV === 'test') {
-        return await targetPromise()
-    }
+	if (process.env.NODE_ENV === 'test') {
+		return targetPromise();
+	}
 
-    const scheduleNextExecution = () => setTimeout(() => executeAndRepeat(targetPromise, milliseconds), milliseconds)
+	const scheduleNextExecution = () => setTimeout(async () => executeAndRepeat(targetPromise, milliseconds), milliseconds);
 
-    try {
-        const result = await targetPromise()
-        scheduleNextExecution()
-        return result
-    } catch (error) {
-        scheduleNextExecution()
-        throw error
-    }
+	try {
+		const result = await targetPromise(); // eslint-disable-line @typescript-eslint/no-unsafe-assignment
+		scheduleNextExecution();
+		return result; // eslint-disable-line @typescript-eslint/no-unsafe-return
+	} catch (error) {
+		scheduleNextExecution();
+		throw error;
+	}
 }
