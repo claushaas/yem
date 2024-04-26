0781c346a5d34a68de59c28e0d5d6a5e12b57c2b
Claus Haas <claus.haas@me.com>
2024-04-26 19:56:45 -0300

    create custom hook for text editor content

---
+++ app/hooks/use-text-editor.hook.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/ban-types */
+import type Quill from 'quill';
+import {type Delta} from 'quill/core';
+import {useEffect, useState} from 'react';
+
+export const useTextEditor = (initialContent?: string | null): [string, React.Dispatch<React.SetStateAction<Quill | null>>] => {
+	const [quill, setQuill] = useState<Quill | null>(null);
+	const [content, setContent] = useState<string>(initialContent ?? '');
+
+	useEffect(() => {
+		if (quill) {
+			quill.on('text-change', () => {
+				setContent(JSON.stringify(quill.getContents()));
+			});
+		}
+	}, [quill]);
+
+	useEffect(() => {
+		if (initialContent && quill) {
+			quill.setContents(JSON.parse(initialContent) as Delta);
+		}
+	}, [quill]); // eslint-disable-line react-hooks/exhaustive-deps
+
+	return [content, setQuill];
+};
