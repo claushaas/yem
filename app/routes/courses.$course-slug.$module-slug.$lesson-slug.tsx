f23fe8e45fb88e2719bfa4404a4fb9ef0743cee7
Claus Haas <claus.haas@me.com>
2024-07-02 12:43:59 -0300

    add lesson activity buttons to lesson page

--- app/routes/courses.$course-slug.$module-slug.$lesson-slug.tsx
+++ app/routes/courses.$course-slug.$module-slug.$lesson-slug.tsx
@@ -1,17 +1,24 @@
 import {Stream} from '@cloudflare/stream-react';
-import {json, type LoaderFunctionArgs} from '@remix-run/node';
-import {useLoaderData, type MetaFunction} from '@remix-run/react';
+import {type LoaderFunctionArgs, unstable_defineLoader as defineLoader, type ActionFunctionArgs} from '@remix-run/node';
+import {
+	Await, Form, useLoaderData, type MetaFunction,
+} from '@remix-run/react';
 import {QuillDeltaToHtmlConverter} from 'quill-delta-to-html';
 import {type OpIterator} from 'quill/core';
-import {type TLessonDataForCache} from '~/cache/populate-lessons-to-cache.js';
+import {Suspense} from 'react';
+import {CheckCircleIcon, BookmarkIcon, HeartIcon} from '@heroicons/react/24/outline';
+import {CheckCircleIcon as SolidCheckCircleIcon, BookmarkIcon as SolidBookmarkIcon, HeartIcon as SolidHeartIcon} from '@heroicons/react/24/solid';
 import {Breadcrumbs} from '~/components/breadcrumbs.js';
 import {VideoPlayer} from '~/components/video-player.js';
 import {CourseService} from '~/services/course.service.server';
+import {LessonActivityService} from '~/services/lesson-activity.service.server';
 import {LessonService} from '~/services/lesson.service.server';
 import {ModuleService} from '~/services/module.service.server';
 import {type TUser} from '~/types/user.type';
-import {logger} from '~/utils/logger.util';
 import {getUserSession} from '~/utils/session.server';
+import {YemSpinner} from '~/components/yem-spinner.js';
+import {type TLessonDataForCache} from '~/cache/populate-lessons-to-cache.js';
+import {type TGetLessonActivityForUser} from '~/types/lesson-activity.type';
 
 export const meta: MetaFunction<typeof loader> = ({data}) => [
 	{title: data!.lesson?.lesson.name ?? 'Aula do Curso do Yoga em Movimento'},
@@ -24,9 +31,11 @@ type LessonLoaderData = {
 	meta: Array<{tagName: string; rel: string; href: string}>;
 	course: {slug: string; name: string} | undefined;
 	module: {slug: string; name: string} | undefined;
+	userLessonActivity: Promise<TGetLessonActivityForUser>;
+	userId: string;
 };
 
-export const loader = async ({request, params}: LoaderFunctionArgs) => {
+export const loader = defineLoader(async ({request, params}: LoaderFunctionArgs) => {
 	const userSession = await getUserSession(request.headers.get('Cookie'));
 	const {
 		'course-slug': courseSlug,
@@ -38,37 +47,77 @@ export const loader = async ({request, params}: LoaderFunctionArgs) => {
 		{tagName: 'link', rel: 'canonical', href: new URL(`/courses/${courseSlug}/${moduleSlug}/${lessonSlug}`, request.url).toString()},
 	];
 
-	try {
-		const {data: lesson} = new LessonService().getBySlugFromCache(moduleSlug!, lessonSlug!, userSession.data as TUser);
-		const {data: module} = new ModuleService().getBySlugFromCache(courseSlug!, moduleSlug!, userSession.data as TUser);
-		const {data: course} = new CourseService().getBySlugFromCache(courseSlug!, userSession.data as TUser);
+	const userId = (userSession.data as TUser).id;
 
-		return json<LessonLoaderData>({
-			lesson,
-			module: {
-				slug: module?.moduleSlug ?? '',
-				name: module?.module.name ?? '',
-			},
-			course: {
-				slug: course?.slug ?? '',
-				name: course?.name ?? '',
-			},
-			meta,
-		});
-	} catch (error) {
-		logger.logError(`Error loading module: ${(error as Error).message}`);
-		return json<LessonLoaderData>({
-			lesson: undefined,
-			meta,
-			module: undefined,
-			course: undefined,
-		});
+	const {data: lesson} = new LessonService().getBySlugFromCache(moduleSlug!, lessonSlug!, userSession.data as TUser);
+	const {data: module} = new ModuleService().getBySlugFromCache(courseSlug!, moduleSlug!, userSession.data as TUser);
+	const {data: course} = new CourseService().getBySlugFromCache(courseSlug!, userSession.data as TUser);
+	const userLessonActivity = new LessonActivityService().getLessonActivityForUser(lessonSlug!, userId);
+
+	return {
+		lesson,
+		module: {
+			slug: module?.moduleSlug ?? '',
+			name: module?.module.name ?? '',
+		},
+		course: {
+			slug: course?.slug ?? '',
+			name: course?.name ?? '',
+		},
+		meta,
+		userLessonActivity,
+	};
+});
+
+export const action = async ({request, params, response}: ActionFunctionArgs) => {
+	const userSession = await getUserSession(request.headers.get('Cookie'));
+	const {
+		'course-slug': courseSlug,
+		'module-slug': moduleSlug,
+		'lesson-slug': lessonSlug,
+	} = params;
+
+	const userId = (userSession.data as TUser).id;
+
+	const formData = await request.formData();
+
+	const action = formData.get('action') as string;
+	const completed = (formData.get('completed') as string) === 'true';
+	const saved = (formData.get('saved') as string) === 'true';
+	const favorited = (formData.get('favorited') as string) === 'true';
+
+	const lessonActivityService = new LessonActivityService();
+
+	switch (action) {
+		case 'complete': {
+			await lessonActivityService.togleLessonCompletedForUser(lessonSlug!, userId, completed);
+			break;
+		}
+
+		case 'save': {
+			await lessonActivityService.togleLessonSavedForUser(lessonSlug!, userId, saved);
+			break;
+		}
+
+		case 'favorite': {
+			await lessonActivityService.togleLessonFavoritedForUser(lessonSlug!, userId, favorited);
+			break;
+		}
+
+		default: {
+			break;
+		}
 	}
+
+	response!.headers.set('Location', `/courses/${courseSlug}/${moduleSlug}/${lessonSlug}`);
+	response!.status = 200;
+
+	return null;
 };
 
 export default function Lesson() {
-	const data = useLoaderData<LessonLoaderData>();
-	const {lesson, module, course} = data;
+	const data = useLoaderData<typeof loader>();
+	const {lesson, module, course, userLessonActivity} = data;
 
 	const {ops} = lesson?.lesson.content ? JSON.parse(lesson?.lesson.content) as OpIterator : {ops: []};
 	const contentConverter = new QuillDeltaToHtmlConverter(ops, {
@@ -78,13 +127,33 @@ export default function Lesson() {
 	return lesson && (
 		<main className='w-full max-w-[95%] sm:max-w-[90%] mx-auto'>
 			<Breadcrumbs data={[
-				[`/courses/${course!.slug}`, course!.name], // Course
-				[`/courses/${course!.slug}/${module!.slug}`, module!.name], // Module
-				[`/courses/${course!.slug}/${module!.slug}/${lesson.lessonSlug}`, lesson.lesson.name], // Lesson
+				[`/courses/${course.slug}`, course.name], // Course
+				[`/courses/${course.slug}/${module.slug}`, module.name], // Module
+				[`/courses/${course.slug}/${module.slug}/${lesson.lessonSlug}`, lesson.lesson.name], // Lesson
 			]}/>
 			<div className='w-full max-w-screen-lg mx-auto'>
-				<section id='title'>
+				<section id='title' className='flex justify-center gap-5 items-center mb-10'>
 					<h1 className='text-center'>{lesson.lesson.name}</h1>
+					<Suspense fallback={<YemSpinner/>}>
+						<Await resolve={userLessonActivity}>
+							{userLessonActivity => (
+								<Form method='post' className='flex justify-center gap-3 items-center'>
+									<input type='text' className='hidden' name='completed' value={userLessonActivity.data?.completed ? 'true' : 'false'}/>
+									<input type='text' className='hidden' name='saved' value={userLessonActivity.data?.saved ? 'true' : 'false'}/>
+									<input type='text' className='hidden' name='favorited' value={userLessonActivity.data?.favorited ? 'true' : 'false'}/>
+									<button type='submit' name='action' value='complete' className='cursor-pointer'>
+										{userLessonActivity.data?.completed ? <SolidCheckCircleIcon className='size-8 stroke-purple-11 fill-purple-11'/> : <CheckCircleIcon className='size-8'/>}
+									</button>
+									<button type='submit' name='action' value='save' className='cursor-pointer'>
+										{userLessonActivity.data?.saved ? <SolidBookmarkIcon className='size-8 stroke-purple-11 fill-purple-11'/> : <BookmarkIcon className='size-8'/>}
+									</button>
+									<button type='submit' name='action' value='favorite' className='cursor-pointer'>
+										{userLessonActivity.data?.favorited ? <SolidHeartIcon className='size-8 stroke-purple-11 fill-purple-11'/> : <HeartIcon className='size-8'/>}
+									</button>
+								</Form>
+							)}
+						</Await>
+					</Suspense>
 				</section>
 
 				{lesson.lesson.videoSourceUrl && (
