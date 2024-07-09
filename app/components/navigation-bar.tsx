4cdd2b350b759a89a3b479e5a68cb55fedf61ad7
Claus Haas <claus.haas@me.com>
2024-07-09 10:35:09 -0300

    fix navigation bar

--- app/components/navigation-bar.tsx
+++ app/components/navigation-bar.tsx
@@ -2,7 +2,6 @@ import {Bars4Icon} from '@heroicons/react/24/outline';
 import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import * as Separator from '@radix-ui/react-separator';
 import {Link, useLocation} from '@remix-run/react';
-import {Button, ButtonPreset, ButtonType} from './button.js';
 import {type TypeUserSession} from '~/types/user-session.type';
 
 type NavigateLinkProprierties = {
