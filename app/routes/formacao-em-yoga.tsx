5b47223e2e05901c89a730423bacdfe51d28ac65
Claus Haas <claus.haas@me.com>
2024-11-13 14:01:29 -0300

    new formation page

--- app/routes/formacao-em-yoga.tsx
+++ app/routes/formacao-em-yoga.tsx
@@ -1,4 +1,5 @@
-import {type LoaderFunctionArgs, json} from '@remix-run/node';
+import {Stream} from '@cloudflare/stream-react';
+import {type LoaderFunctionArgs} from '@remix-run/node';
 import {Link, type MetaFunction} from '@remix-run/react';
 import {Button, ButtonPreset, ButtonType} from '~/components/button.js';
 
@@ -8,7 +9,7 @@ export const meta: MetaFunction<typeof loader> = ({data}) => [
 	...data!.meta,
 ];
 
-export const loader = ({request}: LoaderFunctionArgs) => json<{meta: Array<{tagName: string; rel: string; href: string}>}>({
+export const loader = ({request}: LoaderFunctionArgs) => ({
 	meta: [{tagName: 'link', rel: 'canonical', href: new URL('/formacao-em-yoga', request.url).toString()}],
 });
 
@@ -23,11 +24,23 @@ export default function Formacao() {
 					<Link to='/login' aria-label='Entrar na plataforma do Yoga em Movimento'>
 						<Button type={ButtonType.Button} preset={ButtonPreset.Secondary} text='Entrar'/>
 					</Link>
-					<Button type={ButtonType.Button} preset={ButtonPreset.Primary} text='Começar Agora'/>
+					<Button
+						type={ButtonType.Button}
+						preset={ButtonPreset.Primary}
+						text='Começar Agora'
+					/>
 				</div>
 			</header>
 			<main>
-				<h1>Formação</h1>
+				<h1>Tudo o que você precisa para se tornar uma Instrutora de Yoga, do Zero aos Primeiros Alunos</h1>
+				<Stream
+					controls
+					autoplay
+					preload='auto'
+					className='pt-[56.25%] relative *:absolute *:w-full *:h-full *:top-0 *:left-0 *:inset-0'
+					src='6a99796454a960470c8fd444af82b9fe'
+					responsive={false}
+				/>
 			</main>
 		</>
 	);
