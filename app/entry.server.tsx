404b1b98c07f77af14ee47afc3af534e132a95de
Claus Haas <claus.haas@me.com>
2024-11-26 10:23:40 -0300

    reveal entry files

---
+++ app/entry.server.tsx
@@ -0,0 +1,66 @@
+import {PassThrough} from 'node:stream';
+import {type AppLoadContext, type EntryContext, ServerRouter} from 'react-router';
+import {createReadableStreamFromReadable} from '@react-router/node';
+import {isbot} from 'isbot';
+import {type RenderToPipeableStreamOptions, renderToPipeableStream} from 'react-dom/server';
+
+const ABORT_DELAY = 5000;
+
+export default async function handleRequest( // eslint-disable-line max-params
+	request: Request,
+	responseStatusCode: number,
+	responseHeaders: Headers,
+	routerContext: EntryContext,
+	loadContext: AppLoadContext,
+) {
+	return new Promise((resolve, reject) => {
+		let shellRendered = false;
+		const userAgent = request.headers.get('user-agent');
+
+		// Ensure requests from bots and SPA Mode renders wait for all content to load before responding
+		// https://react.dev/reference/react-dom/server/renderToPipeableStream#waiting-for-all-content-to-load-for-crawlers-and-static-generation
+		const readyOption: keyof RenderToPipeableStreamOptions
+      = (userAgent && isbot(userAgent)) || routerContext.isSpaMode ? 'onAllReady' : 'onShellReady'; // eslint-disable-line @typescript-eslint/prefer-nullish-coalescing
+
+		const {pipe, abort} = renderToPipeableStream(
+			<ServerRouter
+				context={routerContext}
+				url={request.url}
+				abortDelay={ABORT_DELAY}
+			/>,
+			{
+				[readyOption]() {
+					shellRendered = true;
+					const body = new PassThrough();
+					const stream = createReadableStreamFromReadable(body);
+
+					responseHeaders.set('Content-Type', 'text/html');
+
+					resolve(
+						new Response(stream, {
+							headers: responseHeaders,
+							status: responseStatusCode,
+						}),
+					);
+
+					pipe(body);
+				},
+				onShellError(error: unknown) {
+					// eslint-disable-next-line @typescript-eslint/prefer-promise-reject-errors
+					reject(error);
+				},
+				onError(error: unknown) {
+					responseStatusCode = 500;
+					// Log streaming rendering errors from inside the shell.  Don't log
+					// errors encountered during initial shell rendering since they'll
+					// reject and get logged in handleDocumentRequest.
+					if (shellRendered) {
+						console.error(error);
+					}
+				},
+			},
+		);
+
+		setTimeout(abort, ABORT_DELAY);
+	});
+}
