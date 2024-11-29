21123960f9d21b3dc7c41123da1140a63b5eb8d0
Claus Haas <claus.haas@me.com>
2024-11-29 16:38:06 -0300

    edit testimony component

--- app/components/testimony-card.tsx
+++ app/components/testimony-card.tsx
@@ -16,7 +16,7 @@ export function Testimony({
 	return (
 		<Dialog.Root>
 			<Dialog.Trigger asChild>
-				<div className='flex-shrink-0 w-56 sm:w-72 h-48 sm:h-60 p-4 sm:p-6 rounded-md shadow-sm shadow-mauve-11 dark:shadow-mauvedark-3 bg-mauve-4 dark:bg-mauvedark-3 hover:cursor-pointer flex flex-col justify-center gap-2'>
+				<div className='flex-shrink-0 w-56 sm:w-72 h-48 sm:h-60 p-4 sm:p-6 rounded-3xl shadow-sm shadow-mauve-11 dark:shadow-mauvedark-3 bg-mauve-4 dark:bg-mauvedark-3 hover:cursor-pointer flex flex-col justify-center gap-2'>
 					<h1 className='text-center text-xl'>{name}</h1>
 					<p className='text-center italic'>{description}</p>
 					<PlayIcon className='size-10 mx-auto'/>
@@ -26,7 +26,7 @@ export function Testimony({
 			<Dialog.Portal>
 				<Dialog.Overlay className='bg-mauvea-12 fixed inset-0'/>
 
-				<Dialog.Content className='fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] p-3 w-[98%] sm:p-10 sm:w-[600px] lg:w-[850px] xl:w-[1000px] bg-mauve-2 dark:bg-mauvedark-2 rounded-xl flex flex-col justify-center gap-8'>
+				<Dialog.Content className='fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] p-3 w-[98%] sm:p-10 sm:w-[600px] lg:w-[850px] xl:w-[1000px] bg-mauve-2 dark:bg-mauvedark-2 rounded-3xl flex flex-col justify-center gap-8'>
 					<div>
 						<Dialog.Title asChild>
 							<h1>{name}</h1>
