c62a33e0f7040cf8564592790b6158ff018ecaa6
Claus Haas <claus.haas@me.com>
2025-09-21 00:02:24 -0300

    chore: corrigir formatação do arquivo prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig } from 'prisma/config'
+import { defineConfig } from 'prisma/config';
 
 export default defineConfig({
-  migrations: {
-    // seed command movido do package.json
-    seed: 'tsx ./prisma/seed.ts',
-  },
-  schema: './prisma/schema.prisma',
-})
+	migrations: {
+		// seed command movido do package.json
+		seed: 'tsx ./prisma/seed.ts',
+	},
+	schema: './prisma/schema.prisma',
+});
